adb755c08b32bc638d2b6633ffe50cf8b62fe7e7
KingArthvr_ <33429259+TheKingArthvr@users.noreply.github.com>
2022-03-25 08:23:27 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -60,12 +60,12 @@ Array(200).fill().forEach(addStar);
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader().load('../../pictures/space.jpg');
+const spaceTexture = new THREE.TextureLoader().load('../space.jpg');
 scene.background = spaceTexture;
 
 // Avatar
 
-const arthurTexture = new THREE.TextureLoader().load('../pictures/arthur.jpg');
+const arthurTexture = new THREE.TextureLoader().load('../arthur.jpg');
 
 const arthur = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: arthurTexture }));
 
@@ -73,8 +73,8 @@ scene.add(arthur);
 
 // Moon
 
-const moonTexture = new THREE.TextureLoader().load('../pictures/moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('../pictures/normal.jpg');
+const moonTexture = new THREE.TextureLoader().load('../moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('../normal.jpg');
 
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
